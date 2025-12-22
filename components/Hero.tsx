c8a744600ec1e5e78e83e0120265ebad1db5fd68
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2025-12-22 14:11:49 +0330

    create hero

---
+++ components/Hero.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+
+const Hero = () => {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    if (videoRef.current) videoRef.current.playbackRate = 2;
+  }, []);
+
+  return (
+    <section id="hero">
+      <div>
+        <h1>MacBook Pro</h1>
+        <Image src="/title.png" alt="MacBook Title" width="256" height="256" />
+      </div>
+
+      <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+
+      <button>Buy </button>
+
+      <p>From $1599 or $133/mo for 12 months</p>
+    </section>
+  );
+};
+export default Hero;
